5f148599aebc46374b57c03c792f041bc8c02b93
Connor Tumbleson <connor@sourcetoad.com>
2022-11-16 11:37:29 -0500

    style: remove docblock

--- ios/QrScanner.swift
+++ ios/QrScanner.swift
@@ -1,9 +1,1 @@
-//
-//  QrScanner.swift
-//  VisionCameraPluginBarcodeScanner
-//
-//  Created by Kendall Kelly on 11/11/22.
-//  Copyright © 2022 Facebook. All rights reserved.
-//
-
 import Foundation
